6d03fb4327abc5b1670d054eb606b57154c513d6
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2014-11-06 10:25:36 +0700

    Add ApplicationExitController script

---
+++ Assets/Scripts/ApplicationExitController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using PlayerPrefs = PreviewLabs.PlayerPrefs;
+
+public class ApplicationExitController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.Flush();
+    }
+}
