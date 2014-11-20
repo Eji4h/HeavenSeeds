d51ba7f02b2cd29ecc762666ddab3c2b57c2d4dc
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2014-11-20 22:12:40 +0700

    PayakKraiSorn Idle looping - SpinController debug not spin after reactive

--- Assets/ParticleAsset/script/SpinController.cs
+++ Assets/ParticleAsset/script/SpinController.cs
@@ -1,48 +1,43 @@
 using UnityEngine;
 using System.Collections;
 
-public class SpinController : MonoBehaviour {
+public class SpinController : MonoBehaviour 
+{
     public float setRotX;
     public float setRotY;
     public float setRotZ;
     public float stopIn;
     public bool canStop = false;
     public bool isOvertime = false;
-    public float overTime,valClamp;
+    public float overTime, valClamp;
 
-
-    IEnumerator Start()
+    void OnEnable()
     {
-        while (true)
+        StartCoroutine(Spin());
+    }
+
+    IEnumerator Spin()
+    {
+        for (; ; )
         {
             if (isOvertime)
             {
-                if (setRotX != 0)
-                {
-                    if(setRotX <= valClamp)
-                        setRotX += overTime*Time.deltaTime;
-                }
-                if (setRotY != 0)
-                {
-                    if (setRotY <= valClamp)
-                        setRotY += overTime*Time.deltaTime;
-                }
-                if (setRotZ != 0)
-                {
-                    if (setRotZ <= valClamp)
-                        setRotZ += overTime*Time.deltaTime;
-                }
+                if (setRotX != 0 && setRotX <= valClamp)
+                        setRotX += overTime * Time.deltaTime;
+                if (setRotY != 0 && setRotY <= valClamp)
+                        setRotY += overTime * Time.deltaTime;
+                if (setRotZ != 0 && setRotZ <= valClamp)
+                        setRotZ += overTime * Time.deltaTime;
             }
             if (canStop)
             {
-              
                 stopIn -= Time.deltaTime;
                 if (stopIn >= 0)
                     transform.Rotate(setRotX * Time.deltaTime, setRotY * Time.deltaTime, setRotZ * Time.deltaTime);
             }
             else
                 transform.Rotate(setRotX * Time.deltaTime, setRotY * Time.deltaTime, setRotZ * Time.deltaTime);
-            yield return 0;
+            yield return null;
         }
     }
 }
