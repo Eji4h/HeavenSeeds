012001e14cc4bc03108fc56dc129b242edacff55
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-01-08 09:31:02 +0700

    Add SetStandAloneResolution script

---
+++ Assets/Scripts/SetStandAloneResolution.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStandAloneResolution : MonoBehaviour
+{
+    void Awake()
+    {
+#if UNITY_STANDALONE
+        Screen.SetResolution(480, 800, false);
+#endif
+    }
+}
