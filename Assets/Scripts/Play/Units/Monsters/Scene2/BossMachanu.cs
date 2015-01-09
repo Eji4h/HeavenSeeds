ac15b2e8cc05534ee9cf115b8c2f30f17e73f8ed
menstood <mesntood@gmail.com>
2015-01-09 15:13:04 +0700

    Add All Boss Script

---
+++ Assets/Scripts/Play/Units/Monsters/Scene2/BossMachanu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMachanu : Monster {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
