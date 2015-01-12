11a658190608643cffabdc14abb27425f72dd1b1
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-01-12 17:22:31 +0700

    Boss Machanu finish waiting balance

--- Assets/Scripts/Play/Units/Monsters/Scene2/BossMachanu.cs
+++ Assets/Scripts/Play/Units/Monsters/Scene2/BossMachanu.cs
@@ -1,15 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class BossMachanu : Monster {
+public class BossMachanu : Monster
+{
+    enum BossMachanuState
+    {
+        JumpDown,
+        JumpPounded, 
+        Ultimate
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    List<BossMachanuState> listBossMachanuState = new List<BossMachanuState>(3);
+
+    bool isDown = false;
+    int waterCorrect = 0;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        MaxHp = 10000;
+        DamageBase = 176;
+        listBossMachanuState.Add(BossMachanuState.JumpDown);
+        listBossMachanuState.Add(BossMachanuState.JumpPounded);
+        base.Start();
+    }
+
+    protected override void MonsterBehaviour()
+    {
+        if (isDown)
+            thisAnimation.CrossFade("JumpUp");
+        else
+        {
+            if (waterCorrect < 6)
+                thisAnimation.CrossFade(listBossMachanuState[Random.Range(0, listBossMachanuState.Count)].ToString());
+            else
+                thisAnimation.CrossFade("Ultimate");
+        }
+    }
+
+    public void SetIsDownTrue()
+    {
+        isDown = true;
+        isImmortal = true;
+    }
+
+    public void SetIsDownFalse()
+    {
+        isDown = false;
+        isImmortal = false;
+    }
+
+    public void ResetWaterCollect()
+    {
+        waterCorrect = 0;
+    }
+
+    public void IncreaseWaterCollect()
+    {
+        waterCorrect++;
+    }
+
+    public void SendDamgeSpecific(float damageBaseMultiply)
+    {
+        SendDamageToCharacter(damageBaseMultiply * (1 + waterCorrect * 0.05f));
+    }
+
 }
