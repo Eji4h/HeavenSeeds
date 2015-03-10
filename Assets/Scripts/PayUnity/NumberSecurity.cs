3a879f75f055dae5ba04ed75655e598e2452960b
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-03-10 17:37:57 +0700

    Add NumberSecurity script

---
+++ Assets/Scripts/PayUnity/NumberSecurity.cs
@@ -0,0 +1,15 @@
+﻿
+public static class NumberSecurity
+{
+    static int randomNumSecurity;
+
+    public static int RandomNumSecurity
+    {
+        get { return NumberSecurity.randomNumSecurity; }
+    }
+
+    public static NumberSecurity()
+    {
+        randomNumSecurity = UnityEngine.Random.Range(0, 1000);
+    }
+}
